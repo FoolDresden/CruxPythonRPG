d730bbb0fa05ad16af1cdf8421e81eb985b72898
unknown <kumarshreyam0409@gmail.com>
2017-12-31 20:30:32 +0530

    First push

---
+++ RPGgame.py
@@ -0,0 +1,84 @@
+class Character:
+
+    def __init__(self, dmg, health, name):
+        self.dmg = dmg
+        self.health = health
+        self.name = name
+
+    def attack(self, enemy):
+        enemy.health -= self.dmg
+        if enemy.health <= 0:
+            print(enemy.name + " is dead")
+
+    def takeDmg(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            print(self.name + " is dead")
+
+
+class Hero(Character):
+
+    def __init__(self):
+        dmg = 10
+        health = 30
+        name = "Hero"
+        super().__init__(dmg, health, name)
+        self.MAX_HEALTH = 50
+
+    def rest(self):
+        if self.health >= self.MAX_HEALTH:
+            print("No need of rest")
+            return
+        print("Resting...")
+        print("Original health: ", self.health)
+        #self.health = self.MAX_HEALTH
+        self.health += 10
+        if self.health >= self.MAX_HEALTH:
+            self.health = 50
+        print("Rested")
+        print("Final health ", self.health)
+
+
+class Goblin(Character):
+
+    def __init__(self):
+        dmg = 5
+        health = 10
+        name = "Goblin"
+        super().__init__(dmg, health, name)
+
+
+class Orc(Character):
+
+    def __init__(self):
+        dmg = 10
+        health = 20
+        name = "Orc"
+        super().__init__(dmg, health, name)
+
+
+hero = Hero()
+goblin = Goblin()
+orc = Orc()
+hero.rest()
+hero.rest()
+hero.rest()
+print("Goblin attacked")
+goblin.attack(hero)
+print("Hero health: ", hero.health)
+hero.rest()
+print("Orc attacked")
+orc.attack(hero)
+print("Hero health: ", hero.health)
+print("Hero attacked goblin.")
+hero.attack(goblin)
+print("Orc attacked Hero")
+orc.attack(hero)
+print("Hero health: ", hero.health)
+print("Hero attacked Orc thrice")
+hero.attack(orc)
+hero.attack(orc)
+print("Hero is victorious. Final health: ", hero.health)
+hero.rest()
+
+
