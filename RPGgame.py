ae013d088337de2c14e4ee647ab019a3a4134e04
unknown <kumarshreyam0409@gmail.com>
2018-01-04 12:41:02 +0530

    Day 2. First Push

--- RPGgame.py
+++ RPGgame.py
@@ -1,3 +1,5 @@
+import random
+
 class Character:
 
     def __init__(self, dmg, health, name):
@@ -7,12 +9,15 @@ class Character:
 
     def attack(self, enemy):
         enemy.takeDmg(self.dmg)
+        if enemy.health <= 0:
+            enemy.health = 0
+            print(enemy.name + " is dead")
+
 
     def takeDmg(self, damage):
         self.health -= damage
-        if self.health <= 0:
-            self.health = 0
-            print(self.name + " is dead")
+
+
 
 
 class Monster(Character):
@@ -23,12 +28,18 @@ class Monster(Character):
 
 class Hero(Character):
 
+    MAX_HEALTH = 50
+    xp = 0
+    lvl = 1
+    xpToLvls = [10, 20, 30, 40, 50]
+    xp_to_next_lvl = xpToLvls[lvl-1]
+
     def __init__(self, dmg=10, health=30, name="Hero"):
         #dmg = 10
         #health = 30
         #name = "Hero"
         super().__init__(dmg, health, name)
-        self.MAX_HEALTH = 50
+        #self.MAX_HEALTH = 50
 
     def rest(self):
         if self.health >= self.MAX_HEALTH:
@@ -43,6 +54,22 @@ class Hero(Character):
         print("Rested")
         print("Final health ", self.health)
 
+    def lvlup(self):
+        self.xp += 10
+        if self.xp is self.xp_to_next_lvl:
+            self.lvl += 1
+            print("Level up!!! Level is ", self.lvl)
+            self.xp_to_next_lvl = self.xpToLvls[self.lvl - 1]
+            self.xp = 0
+
+    def attack(self, enemy):
+        super().attack(enemy)
+        if enemy.health is 0:
+            self.lvlup()
+
+    def __str__(self):
+        return 'Name: %s\nHealth: %d\nLevel: %d\nXP towards level %d: %d/%d' % (self.name, self.health, self.lvl, (self.lvl +1),  self.xp, self.xp_to_next_lvl)
+
 
 class Goblin(Monster):
 
@@ -51,6 +78,9 @@ class Goblin(Monster):
         #health = 10
         #name = "Goblin"
         super().__init__(dmg, health, name)
+
+    def __str__(self):
+        return 'Name: %s\nHealth: %d' % (self.name, self.health)
 
 
 class Orc(Monster):
@@ -61,9 +91,17 @@ class Orc(Monster):
         #name = "Orc"
         super().__init__(dmg, health, name)
 
+    def __str__(self):
+        return 'Name: %s\nHealth: %d' % (self.name, self.health)
+
+
+def explore():
+    monsterType = random.choices([Goblin(), Orc(), None], [0.4, 0.1, 0.5], k=1)
+    return monsterType[0]
+
 
 if __name__ == "__main__":
-    hero = Hero(10, 30, "Hero")
+    """hero = Hero(10, 30, "Hero")
     goblin = Goblin(5, 10, "Goblin")
     orc = Orc(10, 20, "Orc")
     hero.rest()
@@ -87,7 +125,32 @@ if __name__ == "__main__":
     hero.attack(orc)
     hero.attack(orc)
     print("Hero is victorious. Final health: ", hero.health)
-    hero.rest()
+    hero.rest()"""
+    hero = Hero(10, 30, "Hero")
+    goblin = Goblin(5, 10, "Goblin")
+    orc = Orc(10, 20, "Orc")
+    monster = explore()
+    if monster is not None:
+        print(monster.name+" appeared!")
+        print("Hero attacked monster.")
+        hero.attack(monster)
+        print(monster)
+        print(hero)
+        for i in range(2):
+            if monster.health != 0:
+                print(monster.name + " attacked hero")
+                monster.attack(hero)
+                print(hero.name + " attacked the monster")
+                hero.attack(monster)
+    else:
+        print("No monster found")
+
+
+
+
+
+
+
 
 
 
